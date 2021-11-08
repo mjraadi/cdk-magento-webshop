1d188fdd52d3d2a0b360ee0357a253692eb28f2d
Mohammad <raadi@bitsnbytes.ir>
2021-11-08 23:28:33 +0330

    * installed new dependencies * updated `app.py` file   * include global tags for all resources created by the stack   * read target AWS account ID and region through environment variables * provisioned a VPC which can opt for cost-effectiveness or high-availability based on the environment

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python3
+
+# importing libraries
 import os
 from aws_cdk import core as cdk
-
+# importing stack constructs
 from stacks.vpc import VpcStack
+# importing util functions
+from utils import getBuildConfigs
 
 
+# initiating a CDK app
 app = cdk.App()
-VpcStack(app, "VpcStack")
+
+# reading project configs
+buildConfigs = getBuildConfigs(app)
+
+# setting global tags
+globalTags = buildConfigs["GlobalTags"]
+if globalTags is not None:
+  for key in globalTags:
+    cdk.Tags.of(app).add(key, globalTags[key])
+
+# setting up the environment the stack is provisioned
+_account = os.environ.get(
+  "CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]
+)
+_region = os.environ.get(
+  "CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]
+)
+_env = cdk.Environment(
+  account=_account,
+  region=_region
+)
+
+# main stack name
+stackName = f"{buildConfigs['App']}-{buildConfigs['Environment']}"
+
+# provisioning VPC stack
+VpcStack(app, stackName, env=_env, buildConfigs=buildConfigs)
 
 app.synth()
--- stacks/vpc.py
+++ stacks/vpc.py
@@ -1,9 +1,68 @@
 from aws_cdk import core as cdk
-
+from aws_cdk import aws_ec2 as _ec2
 
 class VpcStack(cdk.Stack):
+  def __init__(self, scope: cdk.Construct, construct_id: str, buildConfigs, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    # reading vpc configs from the project's cdk.json context
+    # TODO: check for noneness
+    cidr = buildConfigs["Parameters"]["VPC"]["CIDR"]
+    cidrMask = buildConfigs["Parameters"]["VPC"]["CIDRMask"]
+    environment = buildConfigs["Environment"]
 
-        # The code that defines your stack goes here
+    # ensure high availability on production environment
+    natGateways = 2 if environment == "prod" else 1
+
+    # defining vpc subnets: public, private, and an isolated
+    # subnet for our DB instances
+    publicSubnetConfig = _ec2.SubnetConfiguration(
+      name="publicSubnet",
+      cidr_mask=cidrMask,
+      subnet_type=_ec2.SubnetType.PUBLIC
+    )
+
+    privateSubnetConfig = _ec2.SubnetConfiguration(
+      name="privateSubnet",
+      cidr_mask=cidrMask,
+      subnet_type=_ec2.SubnetType.PRIVATE
+    )
+
+    isolatedSubnetConfig = _ec2.SubnetConfiguration(
+      name="isolatedSubnet",
+      cidr_mask=cidrMask,
+      subnet_type=_ec2.SubnetType.ISOLATED
+    )
+
+    # creating our vpc that spans over two AZs and has two
+    # NAT gateways for each private subnet to achieve
+    # high-availability
+    _vpc = _ec2.Vpc(
+      self,
+      "vpc",
+      cidr=cidr,
+      max_azs=2,
+      nat_gateways=natGateways,
+      subnet_configuration=[
+        publicSubnetConfig,
+        privateSubnetConfig,
+        isolatedSubnetConfig,
+      ]
+    )
+
+    # assigning our VPC resource to be able to reference it
+    # across stacks
+    self._vpc = _vpc
+
+    # our stack outputs
+    cdk.CfnOutput(
+      self,
+      "vpcOutput",
+      value=_vpc.vpc_id,
+      description="VPC ID",
+      export_name="vpcId"
+    )
+
+  @property
+  def getVpc(self) -> _ec2.IVpc:
+    return self._vpc
